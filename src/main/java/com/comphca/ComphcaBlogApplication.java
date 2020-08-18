bf047edec3326e834070ad509d0d25c8acb279f3
comphca <comphca@gmail.com>
2020-08-19 00:37:32 +0800

    新建的blog项目

---
+++ src/main/java/com/comphca/ComphcaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.comphca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComphcaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComphcaBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comphca/ComphcaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comphca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComphcaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
